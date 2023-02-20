844e9b8f6dd830b59088526e91693d310cfec538
shiba_dog <shiba_dog@outlook.com>
2023-02-20 15:41:30 +0000

    testing memberlist

--- README.md
+++ README.md
@@ -16,3 +16,27 @@
 ```
 
 これは、このコマンドを実行する際に、存在する最新バージョンのimport対象のバージョンを含めた依存情報を `go.mod` に追加し、 `go.sum` にそのバージョンのHASHを保持する。
+
+## memberlist
+
+hashiCorpが作ったクラスタ構築ライブラリを使ってみた。
+
+[memberlistを使ってクラスタリング - ノードの参加と削除](https://qiita.com/octu0/items/808299d232bc003d5e99)
+
+``` bash
+# docker compose up app1
+```
+
+上記のようなコマンドでserver1台が起動する。
+
+その後、別shellでnode2を起動する
+
+``` bash
+# docker compose up app2
+```
+
+こうすると、二代目に起動した app2 が app1 のクラスタに自動参加される。
+
+app2 を `ctrl+c` で終了すると app1 が検知して離れたことを認識する。
+
+これでお互いに死活監視が行えるようになった。
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"rsc.io/quote"
+	"github.com/hashicorp/memberlist"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// https://qiita.com/octu0/items/808299d232bc003d5e99
+	nodeName := flag.String("name", "hoge", "node name")
+	masterNode := flag.String("masterAddr", "127.0.0.1", "master addr")
+	flag.Parse()
+
+	conf := memberlist.DefaultLocalConfig()
+	conf.Name = *nodeName
+
+	list, err := memberlist.Create(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	local := list.LocalNode()
+	log.Printf("%s at %s:%d", local.Name, local.Addr.To4().String(), local.Port)
+
+	log.Printf("wait for other member connections")
+	if *masterNode != local.Addr.To4().String() {
+		if _, err := list.Join([]string{*masterNode}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for _, member := range list.Members() {
+		log.Printf("Member: %s(%s:%d)", member.Name, member.Addr.To4().String(), member.Port)
+	}
+
+	defer func() {
+		log.Printf("bye")
+	}()
+	signal_chan := make(chan os.Signal, 2)
+	signal.Notify(signal_chan, syscall.SIGTERM)
+	signal.Notify(signal_chan, syscall.SIGINT)
+
+	log.Printf("wait for signal: pid=%d", os.Getpid())
+	for {
+		select {
+		case s := <-signal_chan:
+			switch s {
+			case syscall.SIGINT:
+				log.Printf("SIGINT happen. cluter leaving")
+				timeout := 10 * time.Second
+				if err := list.Leave(timeout); err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("cluter left.")
+			case syscall.SIGTERM:
+				log.Printf("SIGTERM happen. bye.")
+				return
+			}
+		}
+	}
 }
